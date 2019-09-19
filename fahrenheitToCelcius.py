d3fb35fc13539787583333d3730edec20ae4a0e7
Patricia De Nardi <dena0034@algonquinlive.com>
2019-09-18 20:51:16 -0400

    add fahreneit to celcius exercice

---
+++ fahrenheitToCelcius.py
@@ -0,0 +1,7 @@
+#this program convert fahrenheit to celcius
+
+degreeFah = float(input("What is the tempeture in Fahrenheit?: "))
+
+degreeCelcius = ( (degreeFah - 32) * 5/9)
+
+print("The degree in Celcius is: ", degreeCelcius)
