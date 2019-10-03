5a58afaa21e2201964df689ecfcac3aeba2911d0
Patricia De Nardi <dena0034@algonquinlive.com>
2019-10-02 22:46:44 -0400

    lab5

--- lab5/labLibrary.py
+++ lab5/labLibrary.py
@@ -1,13 +1,17 @@
 
 from gfxhat import lcd,backlight
+from time import sleep
+from random import randint
 
 
-def light():
-    backlight.set_all(107, 109, 173)
+def setLight(r,g,b):
+    backlight.set_all(r, g, b)
     backlight.show()
 
 
 def verticalLine(x):
+    lcd.clear()
+    lcd.show()
     i = 0
     while(i < 64):
         lcd.set_pixel(x, i, 1)
@@ -15,6 +19,8 @@ def verticalLine(x):
     lcd.show()
 
 def horizontalLine(y):
+    lcd.clear()
+    lcd.show()
     i = 0
     while(i < 64):
         lcd.set_pixel(i, y, 1)
@@ -25,3 +31,45 @@ def horizontalLine(y):
 def clearBacklight():
     backlight.set_all(0,0,0)
     backlight.show()
+
+def stairCase(xPoint, yPoint, width, height, rightLeft, upDown):
+    lcd.clear()
+    lcd.show()
+
+    while xPoint < 128 and xPoint >= 0 and yPoint < 64 and yPoint >= 0:
+        w = 1
+    h = 1
+    while(w < width):
+        lcd.set_pixel(xPoint, yPoint, 1)
+        if(rightLeft == 1):
+            xPoint = xPoint - 1
+        elif(rightLeft ==2):
+            xPoint = xPoint + 1
+    
+        if xPoint > 127 and xPoint < 0 and yPoint > 63 and yPoint < 0:
+            lcd.show()
+            break
+        w = w +1
+   
+    while(h < height):
+        lcd.set_pixel(xPoint, yPoint, 1)
+        if(upDown == 1):
+            yPoint = yPoint - 1
+        elif(upDown == 2):
+            yPoint = yPoint + 1
+    
+        if xPoint > 127 and xPoint < 0 and yPoint > 63 and yPoint < 0:
+            lcd.show()
+            break
+        h = h +1
+
+    lcd.show()
+   
+   
+def randomPixel(num):
+    while True:
+        x = randint(0, 128)
+        y = randint(0, 64)
+        lcd.set_pixel(x, y, 1)
+        lcd.show()
+        sleep(num)
--- lab5/testeMenu.py
+++ lab5/testeMenu.py
@@ -1,50 +1,66 @@
-#from gfxhat import lcd, backlight
-#import labLibrary
+from gfxhat import lcd, backlight
+import labLibrary
 import sys
 
 def mainMenu():
     print( )
-    print("------------ Main Menu -------------------")
-    print( )
-
-    option = input(" Choose:\n 1 to draw a vertical line\n 2 to draw a horizontal line\n 3 to create a staicase\n 4 to display randow pixels\n 5 to reset backlight color\n 6 to exit\n")
+    print(" _________________Main Menu_________________")
+    print("  ||   Select:                           ||\n  ||   1 - change the backlight color    || \n  ||   2 - for a vertical line           ||\n  ||   3 - for a horizontal line         ||\n  ||   4 - create a staicase             ||\n  ||   5 - display randow pixel          ||\n  ||   6 - reset backlight color         ||\n  ||   7 - exit                          ||")
+    print(" ___________________________________________")
+    option = input()
+    
+    
 
     if option == "1":
-        #lcd.clear()
-        #lcd.show()
-        withX = int(input("Give the X point: "))
-        print(withX)
-        #labLibrary.verticalLine(withX)   
+        lcd.clear()
+        lcd.show()
+        r = int(input("Give the r: "))
+        g = int(input("Give the g: "))
+        b = int(input("Give the b: "))
+        labLibrary.setLight(r,g,b)  
         mainMenu()
     elif option == "2":
-        #lcd.clear()
-        #lcd.show()
-        heightY = int(input("Give the Y ponit: "))
-        print(heightY)
-        #labLibrary.horizontalLine(heightY)
+        lcd.clear()
+        lcd.show()
+        widthX = int(input("Give the X point: "))
+        print(widthX)
+        labLibrary.verticalLine(widthX)   
         mainMenu()
     elif option == "3":
-        #lcd.clear()
-        #lcd.show()       
-        print("Something")
+        lcd.clear()
+        lcd.show()
+        heightY = int(input("Give the Y ponit: "))
+        print(heightY)
+        labLibrary.horizontalLine(heightY)
         mainMenu()
     elif option == "4":
-        #lcd.clear()
-        #lcd.show()
-        print("Something")
+        lcd.clear()
+        lcd.show()  
+        xPoint = int(input("Give the x position (from 0 to 127):"))
+        yPoint = int(input("Give the y position (from 0 to 63):"))
+        width = int(input("What is the width?: "))
+        height = int(input("What is the height?: "))
+        stairWay = int(input("Type 1 for left OR 2 for right: "))
+        upDown = int(input("Type 1 for up OR 2 for down: "))   
+        labLibrary.stairCase(xPoint, yPoint, width, height,stairWay, upDown)  
         mainMenu()
     elif option == "5":
-        #lcd.clear()
-        #lcd.show()
-        #labLibrary.clearBacklight()
-        print("Something")
+        lcd.clear()
+        lcd.show()
+        num = float(input("Type the seconds: "))
+        labLibrary.randomPixel(num)
         mainMenu()
     elif option == "6":
-        #lcd.clear()
-        #lcd.show()
-         sys.exit
+        lcd.clear()
+        lcd.show()
+        labLibrary.clearBacklight()
+        mainMenu()
+    elif option == "7":
+        lcd.clear()
+        lcd.show()
+        sys.exit
     else:
-        print("Please type a valid option") 
+        print("Please choose a valid option!") 
         mainMenu()
 
 
